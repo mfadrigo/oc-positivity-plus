18bc0116f42c84cb82477d5952027230bd7d2cba
unknown <catalmm1@uci.edu>
2020-10-13 00:09:52 -0700

    Added model 1 - discretized time

--- analysis/logistic-regression.R
+++ analysis/logistic-regression.R
@@ -1,8 +1,29 @@
+library(lme4)
 source(here::here("data-wrangling", "read-sanatize-oc-covid-data.R"))
-read_line_list()
+all_pcr <- read_all_pcr()
+
+all_pcr$population_density <- scale(all_pcr$population_density, 
+                                   center = TRUE, 
+                                   scale = TRUE)
+all_pcr$med_adj_income <- scale(all_pcr$med_adj_income, 
+                                    center = TRUE, 
+                                    scale = TRUE)
+
+model1 <- glmer(formula = covid_positive ~ age_groups + sex + race + 
+                          med_adj_income + month + population_density + 
+                          (1|zip), 
+              family = binomial, 
+              data = all_pcr, 
+              subset = (month %in% c("3", "4", "5", "6")))
 
 
+source(here::here("analysis", "dans-glm-transformed-ci-function.R"))
+m1_sum <- glmCI(model = model1, transform = TRUE, robust = TRUE)
+m1_sum
+############################################################################################################################################################
 
-model1 <- glm(formula = Positive ~ factor(AgeGroup) + Male + factor(Race) + Income + 
-                                   factor(Month) + PopDen, 
-              family = binomial(logistic), data = all_PCR1)
+###Model diagnostics
+###Worried about multicolinearity due to variables based on zipcode
+#Check vif
+library(car)
+vif(model1)
