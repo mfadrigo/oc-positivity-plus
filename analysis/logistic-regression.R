1e1914e80416972d1ae9a1a041ab6aa591c8ba47
unknown <catalmm1@uci.edu>
2020-10-13 16:56:00 -0700

    standardized zipcode level variables

--- analysis/logistic-regression.R
+++ analysis/logistic-regression.R
@@ -2,19 +2,29 @@ library(lme4)
 source(here::here("data-wrangling", "read-sanitize-oc-covid-data.R"))
 all_pcr <- read_all_pcr()
 
+
 all_pcr$population_density <- scale(all_pcr$population_density, 
                                    center = TRUE, 
                                    scale = TRUE)
 all_pcr$med_adj_income <- scale(all_pcr$med_adj_income, 
                                     center = TRUE, 
                                     scale = TRUE)
+all_pcr$percent_bachelors <- scale(all_pcr$percent_bachelors,
+                                   center = TRUE,
+                                   scale = TRUE)
+all_pcr$percent_insured <- scale(all_pcr$percent_insured,
+                                 center = TRUE,
+                                 scale = TRUE)
 
+
+
+pcr_march_to_june <- all_pcr[all_pcr$posted_month %in% c("3", "4", "5", "6"), ]
 model1 <- glmer(formula = covid_positive ~ age_groups + sex + race + 
-                          med_adj_income + month + population_density + 
+                          med_adj_income + posted_month + population_density + 
                           (1|zip), 
               family = binomial, 
-              data = all_pcr, 
-              subset = (month %in% c("3", "4", "5", "6")))
+              data = pcr_march_to_june,
+              control = glmerControl(optimizer ="bobyqa", optCtrl=list(maxfun=100000)))
 
 
 source(here::here("analysis", "dans-glm-transformed-ci-function.R"))
