420ee26b3363244c76515c7db32519b9f0f971ce
unknown <catalmm1@uci.edu>
2020-10-12 23:39:57 -0700

    save negatives up to first positive

--- data-wrangling/read-sanatize-oc-covid-data.R
+++ data-wrangling/read-sanatize-oc-covid-data.R
@@ -74,55 +74,6 @@ other_test_synonyms <- c("inconclusive",
                          "invalid result", 
                          "specimen unsatisfactory for evaluation")
 
-#read_line_list <- function(line_list_name = "10.05.20 release to UCI team.csv",
-#                           negative_line_list_name = "All ELR PCR tests updated 10.05.20.csv") {
-#  new_deaths_tbl <- read_csv(here::here("data", line_list_name),
-#                             col_types = cols(.default = col_skip(),
-#                                              `DtDeath` = col_date("%Y-%m-%d"))) %>%
-#    drop_na() %>%
-#    transmute(posted_date = `DtDeath`) %>%
-#    count(posted_date, name = "new_deaths") %>%
-#    arrange(posted_date)
-#  
-#  
-#  
-#  neg_line_list <- read_csv(here::here("data", negative_line_list_name),
-#                            col_types = cols(.default = col_skip(),
-#                                             PersonId = col_integer(),
-#                                             Specimen.Collected.Date = col_date("%m-%d-%Y"),
-#                                             Resulted.Organism = col_character())) %>%
-#    filter(!is.na(Resulted.Organism)) %>%
-#    mutate(test_result = fct_collapse(str_to_lower(Resulted.Organism),
-#                                      negative = negative_test_synonyms,
-#                                      positive = positive_test_synonyms,
-#                                      other = other_test_synonyms)) %>%
-#    select(id = PersonId, posted_date = Specimen.Collected.Date, test_result) %>%
-#    filter(posted_date >= lubridate::ymd("2020-01-01")) %>%
-#    group_by(id) %>%
-#    arrange(posted_date) %>%
-#    ungroup()
-#  
-#  if(length(levels(neg_line_list$test_result)) != 3) warning("New test result category not accounted for.")
-#  
-#  first_pos <- neg_line_list %>%
-#    filter(test_result == "positive") %>%
-#    group_by(id) %>%
-#    summarise(first_pos = min(posted_date))
-#  
-#  neg_line_list_filtered <- left_join(neg_line_list, first_pos) %>%
-#    mutate(first_pos = replace_na(first_pos, lubridate::ymd("9999-12-31"))) %>%
-#    filter(posted_date <= first_pos) %>%
-#    select(-first_pos) %>%
-#    distinct()
-#  
-#  neg_line_list_filtered %>%
-#    count(posted_date, test_result) %>%
-#    pivot_wider(names_from = test_result, values_from = n) %>%
-#    full_join(new_deaths_tbl) %>%
-#    replace(is.na(.), 0) %>%
-#    mutate(new_cases = positive, new_tests = negative + positive + other) %>%
-#    select(posted_date, new_cases, new_tests, new_deaths)
-#}
 
 read_all_pcr <- function(start_date = "2020-01-01") {
   pcr_results_original <- read_csv("C:/Users/Catalina Medina/Documents/oc-positivity-plus-outer/All ELR PCR tests updated 10.05.20.csv",
@@ -211,52 +162,14 @@ read_all_pcr <- function(start_date = "2020-01-01") {
   pcr_results_merged$population_density <- pcr_results_merged$population / pcr_results_merged$area_km
   
   
-  
-  
-#  new_deaths_tbl <- read_csv(here::here("data", "oc", line_list_name),
-#                             col_types = cols(.default = col_skip(),
-#                                              `DtDeath` = col_date("%Y-%m-%d"),
-#                                              Zip = col_character())) %>%
-#    drop_na() %>%
-#    select(posted_date = `DtDeath`, zip = Zip) %>%
-#    count(posted_date, zip, name = "new_deaths") %>%
-#    arrange(posted_date)
-  
-  
-#  first_pos <- neg_line_list %>%
-#    filter(test_result == "positive") %>%
-#    group_by(id) %>%
-#    summarise(first_pos = min(posted_date))
-#  
-#  neg_line_list_filtered <- left_join(neg_line_list, first_pos) %>%
-#    mutate(first_pos = replace_na(first_pos, lubridate::ymd("9999-12-31"))) %>%
-#    filter(posted_date <= first_pos) %>%
-#    select(-first_pos) %>%
-#    distinct()
-#  
-#  
-#  oc_zips <- read_csv(here("data", "oc", "oc_zips.csv"),
-#                      col_types = cols(Zip = col_character())) %>%
-#    rename_all(str_to_lower) %>%
-#    group_by(city) %>%
-#    summarize(zip = list(zip),
-#              population = sum(population))
-#  
-#  
-#  neg_line_list_filtered_city <- neg_line_list_filtered %>%
-#    right_join(oc_zips %>% select(-population) %>% unnest(zip)) %>%
-#    count(posted_date, test_result, city) %>%
-#    pivot_wider(names_from = test_result, values_from = n)
-#  
-#  
-#  new_deaths_tbl_city <- new_deaths_tbl %>%
-#    right_join(oc_zips %>% select(-population) %>% unnest(zip)) %>%
-#    drop_na() %>%
-#    count(posted_date, city, wt = new_deaths, name = "new_deaths")
-#  
-#  full_join(neg_line_list_filtered_city, new_deaths_tbl_city) %>%
-#    replace(is.na(.), 0) %>%
-#    mutate(new_cases = positive, new_tests = negative + positive + other) %>%
-#    select(posted_date, city, new_cases, new_tests, new_deaths) %>%
-#    arrange(city, posted_date)
+  first_pos <- pcr_results_merged %>%
+               filter(test_result == "positive") %>%
+               group_by(id) %>%
+               summarise(first_pos = min(posted_date))
+
+  pcr_reduced <- left_join(pcr_results_merged, first_pos) %>%
+    mutate(first_pos = replace_na(first_pos, lubridate::ymd("9999-12-31"))) %>%
+    filter(posted_date <= first_pos) %>%
+    select(-first_pos) %>%
+    distinct()
 }
