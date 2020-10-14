c9b24a854e1340de79c9195ed2cc83405713720f
unknown <catalmm1@uci.edu>
2020-10-14 12:33:04 -0700

    fixed subset issue and centered time

--- analysis/logistic-regression.R
+++ analysis/logistic-regression.R
@@ -7,34 +7,38 @@ source(here::here("analysis", "helpful-data-analysis-functions.R"))
 
 # Change file path for where you saved the all_ELR_PCR_tests_updated file
 all_pcr <- read_all_pcr(file_path = "C:/Users/Catalina Medina/Documents/oc-positivity-plus-outer/All ELR PCR tests updated 10.05.20.csv")
-all_pcr_1 <- all_pcr
+pcr_march_to_june <- all_pcr[all_pcr$posted_month %in% c("3", "4", "5", "6"), ]
 
-all_pcr_1$adj_population_density <- scale(all_pcr_1$population_density, 
+
+pcr_march_to_june$adj_population_density <- scale(pcr_march_to_june$population_density, 
                                    center = TRUE, 
                                    scale = TRUE)
-all_pcr_1$adj_med_income <- scale(all_pcr_1$med_income, 
+pcr_march_to_june$adj_med_income <- scale(pcr_march_to_june$med_income, 
                                     center = TRUE, 
                                     scale = TRUE)
-all_pcr_1$adj_percent_bachelors <- scale(all_pcr_1$percent_bachelors,
+pcr_march_to_june$adj_percent_bachelors <- scale(pcr_march_to_june$percent_bachelors,
                                    center = TRUE,
                                    scale = TRUE)
-all_pcr_1$adj_percent_insured <- scale(all_pcr_1$percent_insured,
+pcr_march_to_june$adj_percent_insured <- scale(pcr_march_to_june$percent_insured,
                                  center = TRUE,
                                  scale = TRUE)
+pcr_march_to_june$adj_time_days <- scale(pcr_march_to_june$time_days,
+                                 center = TRUE,
+                                 scale = FALSE)
 
-all_pcr_1$adj_med_income_quartile <- with(all_pcr_1,
+pcr_march_to_june$adj_med_income_quartile <- with(pcr_march_to_june,
                                  cut(adj_med_income,
                                      breaks = quantile(adj_med_income, 
                                                        probs = seq(0, 1, by = 0.25)),
                                      include.lowest = TRUE,
                                      labels = c("Q1", "Q2", "Q3", "Q4")))
-all_pcr_1$adj_per_bachelors_quartile <- with(all_pcr_1,
+pcr_march_to_june$adj_per_bachelors_quartile <- with(pcr_march_to_june,
                                         cut(adj_percent_bachelors,
                                             breaks = quantile(adj_percent_bachelors, 
                                                               probs = seq(0, 1, by = 0.25)),
                                             include.lowest = TRUE,
                                             labels = c("Q1", "Q2", "Q3", "Q4")))
-all_pcr_1$adj_per_insured_quartile <- with(all_pcr_1,
+pcr_march_to_june$adj_per_insured_quartile <- with(pcr_march_to_june,
                                       cut(adj_percent_insured,
                                           breaks = quantile(adj_percent_insured, 
                                                             probs = seq(0, 1, by = 0.25)),
@@ -42,12 +46,9 @@ all_pcr_1$adj_per_insured_quartile <- with(all_pcr_1,
                                           labels = c("Q1", "Q2", "Q3", "Q4")))
 
 
-pcr_march_to_june <- all_pcr_1[all_pcr_1$posted_month %in% c("3", "4", "5", "6"), ]
-
-
 # Time discretized
 tic()
-model_month <- glmer(formula = covid_positive ~ age_groups_2 + sex + race + 
+model_month <- glmer(formula = covid_positive ~ age_group + sex + race + 
                                adj_per_bachelors_quartile + adj_per_insured_quartile +
                                adj_population_density + adj_med_income +
                                posted_month +
@@ -59,7 +60,7 @@ toc()
 
 # Time discretized with interaction with income
 tic()
-model_month_int <- glmer(formula = covid_positive ~ age_groups_2 + sex + race + 
+model_month_int <- glmer(formula = covid_positive ~ age_group + sex + race + 
                                    adj_per_bachelors_quartile + adj_per_insured_quartile +
                                    adj_population_density + adj_med_income +
                                    posted_month + posted_month * adj_med_income +
@@ -71,7 +72,7 @@ toc()
 
 # Time continuous
 tic()
-model_time <- glmer(formula = covid_positive ~ age_groups_2 + sex + race + 
+model_time <- glmer(formula = covid_positive ~ age_group + sex + race + 
                               adj_per_bachelors_quartile + adj_per_insured_quartile +
                               adj_population_density + adj_med_income +
                               time_days + 
@@ -83,7 +84,7 @@ toc()
 
 # Time continuous with interaction with income
 tic()
-model_time_int <- glmer(formula = covid_positive ~ age_groups_2 + sex + race + 
+model_time_int <- glmer(formula = covid_positive ~ age_group + sex + race + 
                                   adj_per_bachelors_quartile + adj_per_insured_quartile +
                                   adj_population_density + adj_med_income +
                                   time_days + time_days * adj_med_income +
@@ -103,8 +104,17 @@ toc()
 (model_time_int_sum <- compute_exp_ci(model_time_int, 
                                       model_name = "Model with days as predictor and interaction with income"))
 
+ggplot(pcr_march_to_june, aes(x = adj_population_density)) + 
+  geom_histogram()
+ggplot(pcr_march_to_june, aes(x = adj_med_income)) + 
+  geom_histogram()
+ggplot(pcr_march_to_june, aes(x = time_days)) + 
+  geom_histogram()
+ggplot(pcr_march_to_june, aes(x = adj_time_days)) + 
+  geom_histogram()
 
 
+###########################################not working yet###################################################
 # Time continuous gam model with thin plate regression spline
 tic()
 model_gam_time <- gam(covid_positive ~ age_groups_2 + sex + race + 
