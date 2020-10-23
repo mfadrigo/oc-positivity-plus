140b048cc1a55cbd5e702d3f96e511f2a7ddcfeb
unknown <catalmm1@uci.edu>
2020-10-22 20:46:05 -0700

    removed print message from first function

--- analysis/helpful-data-analysis-functions.R
+++ analysis/helpful-data-analysis-functions.R
@@ -1,13 +1,10 @@
 
-compute_ci_logistic <- function(model, alpha = 0.05, model_name = "model", plot_ci = FALSE){
+compute_ci_logistic <- function(model, alpha = 0.05, model_name = "model", plot_ci = TRUE){
   model_summary <- summary(model)
   coeffs <- model_summary$coefficients[, 1]
   std_err <- model_summary$coefficients[, 2]
   ci_lower_bound <- coeffs - qnorm(1 - alpha / 2) * std_err
   ci_upper_bound <- coeffs + qnorm(1 - alpha / 2) * std_err
-  cat(paste(model_name, "\n", 
-            "with ", (1 - alpha) * 100, "% Exponentiated Confidence Intervals (not robust std error) \n", 
-            collapse = ""))
   
   model_sum <- data.frame("odds" = exp(coeffs), 
                           "lower_bound" = exp(ci_lower_bound),
@@ -35,6 +32,35 @@ compute_ci_logistic <- function(model, alpha = 0.05, model_name = "model", plot_
   model_sum
 }
 
+
+compute_ci_gam_logistic <- function(model, alpha = 0.05, model_name, plot_estimates = TRUE) {
+  model_summary <- summary(model)
+  coeffs <- model_summary$p.coeff
+  se <- sqrt(diag(vcov(model, unconditional = TRUE)))[1:length(coeffs)]
+  model_sum <- data.frame("odds" = exp(coeffs),
+                          "lower_bound" = exp(coeffs - qnorm(1 - alpha / 2) * se),
+                          "upper_bound" = exp(coeffs + qnorm(1 - alpha / 2) * se),
+                          "p-value" =  model_summary$p.pv,
+                          "par_names" = factor(names(coeffs)), levels = names(coeffs))
+  
+  if (plot_estimates) {
+    ci_plot <- ggplot(model_sum, 
+                        aes(x = par_names, y = odds),
+                        size = 5, colour = "red") +
+    ylim(min(model_sum$lower_bound), max(model_sum$upper_bound)) +
+    geom_point() +
+    geom_errorbar(aes(ymin = lower_bound, ymax = upper_bound), 
+                  colour = "black") +
+    geom_hline(yintercept = 1, linetype = "dashed", colour = "gray") +
+    theme(axis.text.x = element_text(face = "bold", 
+                                     size = 12, angle = 45)) +
+    ggtitle(model_name)
+    
+    return(list("model_sum" = model_sum, "ci_plot" = ci_plot))
+  }
+  
+  return("model_sum" = model_sum)
+}
 
 
 # par_names <- c("Intercept",
