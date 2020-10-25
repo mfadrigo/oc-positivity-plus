99474a3b16fcd0271d7b924101fa969ac5529068
unknown <catalmm1@uci.edu>
2020-10-25 15:09:56 -0700

    race unknown is valid level and added code to remove inconsistencies in age, sex, and race

--- data-wrangling/read-sanitize-oc-covid-data.R
+++ data-wrangling/read-sanitize-oc-covid-data.R
@@ -86,7 +86,9 @@ read_all_pcr <- function(file_path,
                                              Race = col_character(),
                                              Specimen.Collected.Date = col_date("%m-%d-%Y"),
                                              Resulted.Organism = col_character(),
-                                             Zip = col_character())) 
+                                             Zip = col_character(),
+                                             City = col_character(),
+                                             Performing.Facility.ID = col_character())) 
   
   full_num_data_cases <- nrow(pcr_results_original[(pcr_results_original$Specimen.Collected.Date >= ymd(start_date)) & 
                                                    (pcr_results_original$Specimen.Collected.Date <= ymd(end_date)), ])
@@ -107,24 +109,25 @@ read_all_pcr <- function(file_path,
                                                  asian = "asian",
                                                  black = "black or african american",
                                                  islander = "native hawaiian or other pacific islander",
-                                                 other = c("multiple races", "other"))) %>%
+                                                 other = c("multiple races", "other"),
+                                                 unknown = "unknown")) %>%
                   mutate(race = relevel(factor(str_to_lower(race)), ref = "white")) %>%
-                  mutate(posted_month = factor(month(Specimen.Collected.Date))) %>%
                   mutate(time_days = as.integer(round(difftime(Specimen.Collected.Date, 
                                                                start_date, 
                                                                units = "days")))) %>%
                   select(id = PersonId, 
                          posted_date = Specimen.Collected.Date, 
-                         posted_month,
                          time_days,
                          test_result, 
-                         zip = Zip,
                          age = Age,
                          sex,
-                         race) %>%
+                         race,
+                         zip = Zip,
+                         city = City,
+                         facility = Performing.Facility.ID
+                         ) %>%
                   filter((posted_date >= lubridate::ymd(start_date)) & (posted_date <= lubridate::ymd(end_date))) %>%
                   filter(test_result != "unknown") %>%
-                  filter(race != "unknown") %>%
                   filter(sex != "unknown") %>%
                   mutate(zip = str_sub(zip, end = 5)) %>%
                   drop_na() %>%
@@ -134,23 +137,37 @@ read_all_pcr <- function(file_path,
   
   
   if(length(levels(pcr_results_adjusted$test_result)) != 3) warning("New test result category not accounted for.")
-  
-  # Refactor the sex and race because unknown groups were dropped
-  pcr_results_adjusted$race <- ordered(pcr_results_adjusted$race, 
-                                       levels = c("white", 
-                                                  "american_indigenous", 
-                                                  "asian", 
-                                                  "black", 
-                                                  "islander",
-                                                  "other"))
-  pcr_results_adjusted$sex <- ordered(pcr_results_adjusted$sex,
-                                      levels = c("female", 
-                                                 "male"))
-  
 
   pcr_results_adjusted$covid_positive <- ifelse(pcr_results_adjusted$test_result == "positive", 1, 0)
   
-  first_pos <- pcr_results_adjusted %>%
+  
+  # Extract df of all observations with an id that has inconsistencies for the demographic variables (age, sex, race)
+  pcr_rep_id <- pcr_results_adjusted[duplicated(pcr_results_adjusted$id, fromLast=TRUE) | 
+                                       duplicated(pcr_results_adjusted$id), ]
+
+  pcr_rep_id2 <- pcr_rep_id %>%
+    group_by(id) %>% 
+    mutate(reasonable_ages = diff(range(age)) <= 1) %>%
+    mutate(identical_race = n_distinct(race) == 1) %>%
+    mutate(identical_sex = n_distinct(sex) == 1) %>%
+    ungroup()
+  
+  pcr_inconsistent <- data.frame(pcr_inconsistent[!pcr_inconsistent$reasonable_ages | 
+                                                  !pcr_inconsistent$identical_sex | 
+                                                  !pcr_inconsistent$identical_race, ])
+  
+  inconsistent_counts <- c(length(unique(pcr_inconsistent$id[(!pcr_inconsistent$reasonable_ages)])),
+                           length(unique(pcr_inconsistent$id[(!pcr_inconsistent$identical_sex)])),
+                           length(unique(pcr_inconsistent$id[(!pcr_inconsistent$identical_race)])))
+  names(inconsistent_counts) <- c("number_id_age_inconsistencies", 
+                                  "number_id_sex_inconsistencies",
+                                  "number_id_race_inconsistencies")
+  
+  pcr_results_consistent <- pcr_results_adjusted[!(pcr_results_adjusted$id %in% pcr_inconsistent$id), ]
+  
+  
+  # Need to keep all observations for a person up to and including their first positive
+  first_pos <- pcr_results_consistent %>%
     filter(test_result == "positive") %>%
     group_by(id) %>%
     summarise(first_pos = min(posted_date))
@@ -162,6 +179,7 @@ read_all_pcr <- function(file_path,
     distinct()
   
   
+  # Add zip code level data and merge with pcr results
   zip_area_oc <- read_csv(here::here("data", "zip-area2.csv"),
                           col_types = cols(.default = col_skip(),
                                            NAME = col_character(),
@@ -213,6 +231,7 @@ read_all_pcr <- function(file_path,
   
   zip_data_merged <- merge(x = zip_data_merged, y = zip_insurance_oc, by = "zip")
 
+  
   #23 rows in 92678 zipcode we don't have area data for
   pcr_results_reduced$old_zip <- pcr_results_reduced$zip
   pcr_results_reduced$zip[pcr_results_reduced$old_zip == "92678"] <- "92679"
@@ -223,13 +242,17 @@ read_all_pcr <- function(file_path,
   
   missing_counts <- c(full_num_data_cases , 
                       full_num_data_cases - nrow(pcr_results_adjusted), 
-                      nrow(pcr_results_adjusted) - nrow(pcr_results_reduced),
+                      nrow(pcr_results_adjusted) - nrow(pcr_results_consistent),
+                      nrow(pcr_results_consistent) - nrow(pcr_results_reduced),
                       nrow(pcr_results_reduced) - nrow(pcr_results_merged))
   names(missing_counts) <- c("full_num_data_cases", 
                              "num_na_cases_removed", 
+                             "num_inconsistent_cases_removed",
                              "num_consecutive_cases_removed",
                              "num_bad_zip_cases_removed")
   
+  
+  # Group and scale variables 
   age_breaks <- c(0, 5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 200)
   age_labels <- c("0-4","5-9","10-14","15-19","20-24","25-29","30-34","35-39",
                   "40-49","50-59","60-69","70-79","80+")
@@ -278,8 +301,10 @@ read_all_pcr <- function(file_path,
                                                 include.lowest = TRUE,
                                                 labels = c("Q1", "Q2", "Q3", "Q4")))
   
+
   
   list("pcr_results_merged" = pcr_results_merged, 
        "zip_data_merged" = zip_data_merged, 
-       "counts" = missing_counts)
+       "counts" = missing_counts,
+       "inconsistencies" = inconsistent_counts)
 }
